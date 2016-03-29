8bdae0b2ce6997c833a62b82f17f98e781c36f7b
susanlovaglio <susan.lovaglio@gmail.com>
2016-03-29 15:42:25 -0400

    furniture interactions branch initial commit

---
+++ TeamPopcorn/TeamPopcorn/Furniture.m
@@ -0,0 +1,13 @@
+//
+//  Furniture.m
+//  TeamPopcorn
+//
+//  Created by susan lovaglio on 3/29/16.
+//  Copyright © 2016 Popcorn. All rights reserved.
+//
+
+#import "Furniture.h"
+
+@implementation Furniture
+
+@end
