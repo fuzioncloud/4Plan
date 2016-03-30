157482221222375ed2225425ce3e6eee2e046e88
susanlovaglio <susan.lovaglio@gmail.com>
2016-03-29 20:36:39 -0400

    logic for furniture moving with pan and rotates with two fingers

--- TeamPopcorn/TeamPopcorn/ViewController.m
+++ TeamPopcorn/TeamPopcorn/ViewController.m
@@ -9,6 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *sofa;
+@property (weak, nonatomic) IBOutlet UIImageView *table;
 
 @end
 
@@ -16,12 +18,29 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    UIPanGestureRecognizer *panGestureRecognizerSofa = [[UIPanGestureRecognizer alloc]initWithTarget:self action:@selector(moveFurniture:)];
+    [self.sofa addGestureRecognizer:panGestureRecognizerSofa];
+    
+    UIRotationGestureRecognizer *rotationGestureRecognizerSofa = [[UIRotationGestureRecognizer alloc]initWithTarget:self action:@selector(rotateFurniture:)];
+    [self.sofa addGestureRecognizer:rotationGestureRecognizerSofa];
+    
+    UIPanGestureRecognizer *panGestureRecognizerTable = [[UIPanGestureRecognizer alloc]initWithTarget:self action:@selector(moveFurniture:)];
+    [self.table addGestureRecognizer:panGestureRecognizerTable];
+    
+    UIRotationGestureRecognizer *rotationGestureRecognizerTable = [[UIRotationGestureRecognizer alloc]initWithTarget:self action:@selector(rotateFurniture:)];
+    [self.table addGestureRecognizer:rotationGestureRecognizerTable];
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)moveFurniture:(UIPanGestureRecognizer*)panGestureRecognizer{
+    
+    CGPoint touchLocation = [panGestureRecognizer locationInView:self.view];
+    panGestureRecognizer.view.center = touchLocation;
 }
 
+-(void)rotateFurniture:(UIRotationGestureRecognizer*)rotateGestureRecognizer{
+    
+    rotateGestureRecognizer.view.transform = CGAffineTransformRotate(rotateGestureRecognizer.view.transform, rotateGestureRecognizer.rotation);
+}
 @end
