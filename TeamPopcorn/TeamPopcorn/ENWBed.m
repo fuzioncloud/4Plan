283a0d9ba724bc0d0b8922c1e6f26ebc7296fd82
eoinwhitney <whitneyeoin@gmail.com>
2016-03-30 16:03:31 -0400

    added switches

--- TeamPopcorn/TeamPopcorn/ENWBed.m
+++ TeamPopcorn/TeamPopcorn/ENWBed.m
@@ -17,59 +17,52 @@
     return self;
 }
 
--(instancetype)initWithBedSize: (BedSize) bedSize {
-    self.bedSize = bedSize;
-    if (bedSize == Twin) {
-        self.name=@"twin";
-        self=[self initWithWidth:38 length:75 height:20 image:[UIImage imageNamed:@"basicBed.png"] weight:bedWeight];
-        self.bedSize=bedSize;
-        return self;
-    }
-    if (bedSize==Full) {
-        self.name=@"full";
-        self=[self initWithWidth:53 length:75 height:20 image:[UIImage imageNamed:@"basicBed.png"] weight:bedWeight];
-        self.bedSize=bedSize;
-        return self;
-    }
-    if (bedSize==Queen) {
-        self.name=@"queen";
-        self=[self initWithWidth:60 length:80 height:20 image:[UIImage imageNamed:@"basicBed.png"] weight:bedWeight];
-        self.bedSize=bedSize;
-        return self;
-    }
-    if (bedSize==King) {
-        self.name=@"king";
-        self=[self initWithWidth:76 length:80 height:20 image:[UIImage imageNamed:@"basicBed.png"]
-              weight:bedWeight];
-        self.bedSize=bedSize;
-        return self;
-    }
-    if (bedSize==CaliforniaKing) {
-        self.name=@"california king";
-        self=[self initWithWidth:72 length:84 height:20 image:[UIImage imageNamed:@"basicBed.png"] weight:bedWeight];
-        self.bedSize=bedSize;
-        return self;
-    }
-    if (bedSize==Futon) {
-        self.name=@"futon";
-        self=[self initWithWidth:53 length:75 height:20 image:[UIImage imageNamed:@"basicBed.png"] weight:bedWeight];
-        self.bedSize=bedSize;
-        return self;
-    }
-    if (bedSize==Crib) {
-        self.name=@"crib";
-        self=[self initWithWidth:30 length:50 height:20 image:[UIImage imageNamed:@"basicBed.png"] weight:bedWeight];
-        self.bedSize=bedSize;
-        return self;
+-(instancetype)initWithBedSize:(BedSize)bedSize {
+    
+    switch (bedSize) {
+        case Twin:
+            self.name=@"twin";
+            self=[self initWithWidth:38 length:75 height:20 image:[UIImage imageNamed:@"basicBed.png"] weight:bedWeight];
+            self.bedSize=bedSize;
+            return self;
+            
+        case Full:
+            self.name=@"full";
+            self=[self initWithWidth:53 length:75 height:20 image:[UIImage imageNamed:@"basicBed.png"] weight:bedWeight];
+            self.bedSize=bedSize;
+            return self;
+            
+        case Queen:
+            self.name=@"queen";
+            self=[self initWithWidth:60 length:80 height:20 image:[UIImage imageNamed:@"basicBed.png"] weight:bedWeight];
+            self.bedSize=bedSize;
+            return self;
+            
+        case King:
+            self.name=@"king";
+            self=[self initWithWidth:76 length:80 height:20 image:[UIImage imageNamed:@"basicBed.png"]
+                              weight:bedWeight];
+            self.bedSize=bedSize;
+            return self;
+            
+        case CaliforniaKing:
+            self.name=@"california king";
+            self=[self initWithWidth:72 length:84 height:20 image:[UIImage imageNamed:@"basicBed.png"] weight:bedWeight];
+            self.bedSize=bedSize;
+            return self;
+            
+        default:
+            
+            self=[self initWithWidth:38 length:75 height:20 image:[UIImage imageNamed:@"basicBed.png"] weight:bedWeight];
+            self.bedSize=bedSize;
+            return self;
+
+            
     }
     
-    else {
-        self=[self initWithWidth:38 length:75 height:20 image:[UIImage imageNamed:@"basicBed.png"] weight:bedWeight];
-        self.bedSize=bedSize;
-        return self;
-    }
-
 }
+
+
 
 +(instancetype)twinBed {
     return [[self alloc]initWithBedSize:Twin];
--- TeamPopcorn/TeamPopcorn/ENWChair.m
+++ TeamPopcorn/TeamPopcorn/ENWChair.m
@@ -20,44 +20,46 @@
 }
 
 -(instancetype)initWithChairstlye:(chairStyle)chairStyle {
-    self.chairStyle=chairStyle;
-    if (chairStyle==basicChair) {
-        self.name=@"basic";
-        self=[self initWithWidth:chairWidth length:chairLength height:chairHeight image:[UIImage imageNamed:@"basicChair.png"] weight:chairWeight];
-        self.chairStyle=chairStyle;
-        return self;
-    }
-    if (chairStyle==campingChair) {
-        self.name=@"camping";
-        self=[self initWithWidth:chairWidth length:chairLength height:chairHeight image:[UIImage imageNamed:@"campingChair.png"] weight:chairWeight];
-        self.chairStyle=chairStyle;
-        return self;
-    }
-    if (chairStyle==lazyboyChair) {
-        self.name=@"lazyboy";
-        self=[self initWithWidth:chairWidth length:chairLength height:chairHeight image:[UIImage imageNamed:@"lazyboyChair.png"] weight:chairWeight];
-        self.chairStyle=chairStyle;
-        return self;
-    }
-    if (chairStyle==officeChair) {
-        self.name=@"office";
-        self=[self initWithWidth:chairWidth length:chairLength height:chairHeight image:[UIImage imageNamed:@"officeChair.png"] weight:chairWeight];
-        self.chairStyle=chairStyle;
-        return self;
-    }
-    if (chairStyle==rockingChair) {
-        self.name=@"rocking";
-        self=[self initWithWidth:chairWidth length:chairLength height:chairHeight image:[UIImage imageNamed:@"rockingChair.png"] weight:chairWeight];
-        self.chairStyle=chairStyle;
-        return self;
-    }
-    else {
-        self=[self initWithWidth:chairWidth length:chairLength height:chairHeight image:[UIImage imageNamed:@"basicChair.png"] weight:chairWeight];
-        self.chairStyle=chairStyle;
-        return self;
-        
-    }
     
+    switch (chairStyle) {
+        case basicChair:
+            self.name=@"basic";
+            self=[self initWithWidth:chairWidth length:chairLength height:chairHeight image:[UIImage imageNamed:@"basicChair.png"] weight:chairWeight];
+            self.chairStyle=chairStyle;
+            return self;
+            
+        case campingChair:
+            self.name=@"camping";
+            self=[self initWithWidth:chairWidth length:chairLength height:chairHeight image:[UIImage imageNamed:@"campingChair.png"] weight:chairWeight];
+            self.chairStyle=chairStyle;
+            return self;
+            
+        case lazyboyChair:
+            self.name=@"lazyboy";
+            self=[self initWithWidth:chairWidth length:chairLength height:chairHeight image:[UIImage imageNamed:@"lazyboyChair.png"] weight:chairWeight];
+            self.chairStyle=chairStyle;
+            return self;
+            
+        case officeChair:
+            self.name=@"office";
+            self=[self initWithWidth:chairWidth length:chairLength height:chairHeight image:[UIImage imageNamed:@"officeChair.png"] weight:chairWeight];
+            self.chairStyle=chairStyle;
+            return self;
+            
+        case rockingChair:
+            self.name=@"rocking";
+            self=[self initWithWidth:chairWidth length:chairLength height:chairHeight image:[UIImage imageNamed:@"rockingChair.png"] weight:chairWeight];
+            self.chairStyle=chairStyle;
+            return self;
+            
+        default:
+        
+            self=[self initWithWidth:chairWidth length:chairLength height:chairHeight image:[UIImage imageNamed:@"basicChair.png"] weight:chairWeight];
+            self.chairStyle=chairStyle;
+            return self;
+            
+    }
+      
 }
 
 +(instancetype)basicChair{
