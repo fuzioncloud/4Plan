d85334f67c2d9d87d94801288566da0f214c8c6d
mvargas2193 <mvargas2193@gmail.com>
2016-04-11 16:03:01 -0400

    coreData

--- TeamPopcorn/TeamPopcorn/TPCEnterRoomDimensionViewController.m
+++ TeamPopcorn/TeamPopcorn/TPCEnterRoomDimensionViewController.m
@@ -7,7 +7,6 @@
 //
 
 #import "TPCEnterRoomDimensionViewController.h"
-#import "TPCRoomLayoutViewController.h"
 #import "TPCMainViewController.h"
 #import "TPCStateManager.h"
 
@@ -37,6 +36,9 @@
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     
     [[self view] endEditing:TRUE];
+    
+    
+    
     
 }
 
